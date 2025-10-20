31fdddb07dfee6aa4fd285bc0eb60eb5c94611b3
Juan León <depechejuan@gmail.com>
2025-10-20 19:03:09 +0200

    Funcionalidad 1 termiada - Test finalizados

---
+++ JLM_CEE/Matematicas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLM_CEE
+{
+    public static class Matematicas
+    {
+        public static long CalcularFactorial(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- JLM_CEE_Test/Test1.cs
+++ JLM_CEE_Test/Test1.cs
@@ -1,12 +1,19 @@
-﻿namespace JLM_CEE_Test
+﻿using JLM_CEE;
+
+namespace JLM_CEE_Test
 {
     [TestClass]
     public sealed class Test
     {
-        [TestMethod]
-        public void TestMethod()
+        [DataTestMethod]
+        [DataRow(5, 120)]
+        [DataRow(3, 6)]
+        [DataRow(10, 3628800)]
+        [DataRow(20, 2432902008176640000)]
+        public void CalcularFactorial(int n, long esperado)
         {
-
+            long result = Matematicas.CalcularFactorial(n);
+            Assert.AreEqual(esperado, result);
         }
     }
 }
