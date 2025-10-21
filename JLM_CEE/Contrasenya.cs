382b242ff1cc49328ae46e369240bcb603a5757d
CarlosEspEsc <cespinosaescrig@gmail.com>
2025-10-21 19:24:43 +0200

    Generamos nuevo test y creamos nueva clase para implementar la funcionalidadç

---
+++ JLM_CEE/Contrasenya.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLM_CEE
+{
+    public static class Contrasenya
+    {
+        public static bool ContrasenyaCorrecta(string contrasenya)
+        {
+            return true;
+        }
+    }
+}
